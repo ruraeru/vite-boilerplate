7fb02621d4f7aaeb61266a447cd8ee2e1f83bac0
ruraeru <f93335383@gmail.com>
2025-09-21 16:26:05 +0900

    프로젝트 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-black w-screen h-screen text-avocado-100">
-      Hello World! Count: {count}
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+    <div className="w-screen h-screen flex flex-col gap-4 items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-extrabold">Hello World!</h1>
+      <p className="font-extrabold text-4xl text-yellow-500">{count}</p>
+      <div className="flex gap-4 *:bg-yellow-50 text-black *:p-3 *:hover:bg-yellow-500 *:cursor-pointer">
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
     </div>
   );
 }
